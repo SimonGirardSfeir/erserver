5cd2971438873d5e6357b00b9a2bcc5316dfb9a9
Simon Girard <simon.girard1991@gmail.com>
2023-10-16 17:26:51 +0200

    Introduce Mockito

--- src/test/java/erserver/modules/dependencies/StaffAssignmentManagerTest.java
+++ src/test/java/erserver/modules/dependencies/StaffAssignmentManagerTest.java
@@ -1,48 +1,39 @@
 package erserver.modules.dependencies;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class StaffAssignmentManagerTest {
+    @Mock
+    StaffProvider staffProvider;
+    @Mock
+    BedProvider bedProvider;
+
+    StaffAssignmentManager staffAssignmentManager;
 
     @Test
     void physiciansAndResidentsReturnForPhysiciansOnDuty() {
         // Arrange
-        StaffProviderDouble staffProviderDouble = new StaffProviderDouble();
-        staffProviderDouble.addStaffMemberToReturn(new Staff(1, "John Doctor", StaffRole.DOCTOR));
-        staffProviderDouble.addStaffMemberToReturn(new Staff(2, "Frank Resident", StaffRole.RESIDENT));
-        staffProviderDouble.addStaffMemberToReturn(new Staff(3, "Mary Nurse", StaffRole.NURSE));
-        StaffAssignmentManager manager = new StaffAssignmentManager(staffProviderDouble, new BedProviderDouble());
+        Staff staff1 = new Staff(1, "John Doctor", StaffRole.DOCTOR);
+        Staff staff2 = new Staff(2, "Frank Resident", StaffRole.RESIDENT);
+        Staff staff3 = new Staff(3, "Mary Nurse", StaffRole.NURSE);
+        when(staffProvider.getShiftStaff()).thenReturn(List.of(staff1, staff2, staff3));
+        when(bedProvider.getAllBeds()).thenReturn(new ArrayList<>());
+        staffAssignmentManager = new StaffAssignmentManager(staffProvider, bedProvider);
         // Act
-        List<Staff> physiciansOnDuty = manager.getPhysiciansOnDuty();
+        List<Staff> physiciansOnDuty = staffAssignmentManager.getPhysiciansOnDuty();
         // Assert
         Staff expectedStaff1 = new Staff(1, "John Doctor", StaffRole.DOCTOR);
         Staff expectedStaff2 = new Staff(2, "Frank Resident", StaffRole.RESIDENT);
         assertThat(physiciansOnDuty).isEqualTo(List.of(expectedStaff1, expectedStaff2));
-    }
-
-    private static class StaffProviderDouble implements StaffProvider {
-        private final List<Staff> staffToReturn;
-        public StaffProviderDouble() {
-            this.staffToReturn = new ArrayList<>();
-        }
-        public void addStaffMemberToReturn(Staff staff) {
-            staffToReturn.add(staff);
-        }
-        @Override
-        public List<Staff> getShiftStaff() {
-            return staffToReturn;
-        }
-    }
-
-    private static class BedProviderDouble implements BedProvider {
-        @Override
-        public List<Bed> getAllBeds() {
-            return new ArrayList<>();
-        }
     }
 }
